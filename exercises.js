22c5fbf9b427c17fc1442df51993080b5419b8df
meowmeowcomputers <ryansimonleon@gmail.com>
2017-05-19 14:47:01 -0500

    Completed Array exercises up to sorting

--- exercises.js
+++ exercises.js
@@ -219,5 +219,171 @@ function justPos (array) {
 //Matrix Addition
 
 function matrixAdd (mat1, mat2) {
-  
+  var newMat = [[],[]];
+  for (let x = 0; x < mat1.length; x++) {
+    for (let y = 0; y < mat2.length; y++) {
+      newMat[x][y] = mat1[x][y] + mat2[x][y];
+    }
+  }
+  console.log(newMat);
 }
+
+//Matrix Multiplication
+
+//Rock Paper Scissors
+
+function rockPaperScissors (p1, p2) {
+  if (p1 == 'rock') {
+    if p2 ==
+  }
+  else if (p1 == p2) {
+      console.log('A draw!')
+  }
+}
+
+//Positive Numbers
+// Write a function which takes an array of numbers as input and returns a new
+// array containing only the positive numbers in the given array
+
+var b = [1, -2, -3, 4 , -5, 6, -7]
+
+function isPositive (x) {
+  return x >= 0;
+}
+
+b.filter(isPositive);
+
+// Even Numbers
+//
+// Write a function which takes an array of numbers as input and returns a new
+// array containing only the even numbers in the given array.
+
+var a = [1, 2, 3, 4 ,5 ,6 ,7]
+
+function isEven (x) {
+  return x % 2 == 0;
+}
+
+a.filter(isEven);
+
+// Square the Numbers
+//
+// Write a function which takes an array of numbers as input and returns a
+// new array containing result of squaring each of the numbers in the given array
+// by two. Example: squareTheNumbers([1, 2, 3]) should give [1, 4, 9].
+
+var a = [1, 2, 3, 4];
+function squareNum (n) {
+  return n * n;
+}
+var aSquared = a.map(squareNum);
+
+// Cities 1
+//
+// Write a function which takes an array of city objects
+
+var cities = [
+  { name: 'Los Angeles', temperature: 60.0},
+  { name: 'Atlanta', temperature: 52.0 },
+  { name: 'Detroit', temperature: 48.0 },
+  { name: 'New York', temperature: 80.0 }
+];
+
+cities.filter(function (x) {
+  return x.temperature < 70;
+});
+
+// Cities 2
+//
+// Write a function which takes an array of city objects like the above
+// problem as input and returns an array of the cities names.
+cities.map(function (x) {
+  return x.name;
+});
+
+// Good Job!
+// Print out 'Good Job, {{name}}!' for each person's name, one on a line.
+
+var people = [
+  'Dom',
+  'Lyn',
+  'Kirk',
+  'Autumn',
+  'Trista',
+  'Jesslyn',
+  'Kevin',
+  'John',
+  'Eli',
+  'Juan',
+  'Robert',
+  'Keyur',
+  'Jason',
+  'Che',
+  'Ben'
+];
+
+for (var attribute in people) {
+  var x = people[attribute];
+  console.log(`Hello ${x}`);
+}
+
+// Sort an array
+//
+// Given an array of strings such the array of names given in the previous problem,
+//  sort them by alphabetically order.
+
+people.sort();
+
+// Sort an array, 2
+//
+// Sort the same array, but not by alphabetically order, but by how long each name
+//  is, shortest name first.
+
+people.sort(function(x, y) {
+  if (x.length > y.length) {return 1; }
+  else if (x.length < y.length) { return -1; }
+  return 0;
+  }
+);
+
+// Sort an array, 3
+var arr = [
+  [1, 3, 4],
+  [2, 4, 6, 8],
+  [3, 6]
+];
+
+arr.sort(function(x,y) {
+  if
+  (x.reduce(
+    function (a, b)
+      {return a + b;}
+    )
+    <
+  y.reduce(
+    function (a,b)
+      {return a+b;}
+    )
+  ) {
+    return 1;
+  }
+  else if
+  (x.reduce(
+    function (a, b)
+      {return a+b;}
+    )
+      >
+  y.reduce(
+    function (a,b)
+      {return a+b;}
+    )
+  ) {
+    return -1;
+  }
+  else {
+  return 0;
+    }
+  }
+);
+
+//3 times
